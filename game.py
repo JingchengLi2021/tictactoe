b3f4b8eefc8d5f73fcc2c6704febeb0bdf73f1ad
JingchengLi <jingchengli131@gmail.com>
2022-01-03 10:40:45 -0600

    complete code

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 # first we need to create a class for the game
+import player
 class tictactoe:
     # for a game we need a game board
     # we need two players, represented by letter "O" and "X"
@@ -6,16 +7,11 @@ class tictactoe:
     # Instead, I should define below (the original status of the board, every spot is empty)
     # We don't need to input letter in the game. So the class game is like a game current status, letter is a attribute for the player.
     def __init__(self): # board should be a list storing the data for each spot
-        self.letter = letter
-        self.board = board
+        self.board = [' ' for _ in range(9)]
+        self.winner = None # Initial status of the game , no winner
 
-    @staticmethod
-    def gameboard():
-        #since the board is 3*3, create a 9-element list
-        emptyboard = ['' for _ in range(9)]
-        # divid the list into 3 rows
-        emptyboard = [[' ' for _ in range(3)] for _ in range(3)]
-        for row in emptyboard:
+    def print_currentboard(self):
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
             print('| ' + ' | '.join(row) + ' |')
 
 
@@ -36,45 +32,87 @@ class tictactoe:
     
     # create a method for take a move. parameter should be self.board , self.letter , player
 
-    def take_a_move(self,player):
+    def take_a_move(self,square,letter):
+        # take_a_move is to update the self.board. The parameter required is letter --- the symbol which will fill the spot
+        # and square which is the spot gonna be filled
+        # For this method , we don't need return anything for taking a move. It just updates the board
+        # In this case, we can return a boolean for checking if the take_a_move did happen
+        if self.board[square] == ' ':    
+            self.board[square] = letter
+            if self.win(square,letter):
+                self.winner = letter
+            return True
+        else:
+            return False    
 
 
 
 
 
     # create a method to check if the move leads to a win
-    def win(self,letter):
+    def win(self,square,letter):
 
-        # check rows 
-        for i in range(3):
-            if all(self.board[j] == letter for j in range(3*i,3*(i+1))):
-                print('player ' + letter + ' wins!' )
+        # check rows, we only check the row where the square locates
+        row_ind = square // 3
+        row_list = self.board[3 * row_ind: 3 * (row_ind + 1)]
+        if all([spot == letter for spot in row_list]):
+            return True
         
         # check columns
-
-        for i in range(3):
-            if all(self.board[j] == letter for j in [i,3*(i+1),3*(i+2)])
+        column_ind = square % 3
+        column_list = [self.board[column_ind + 3 * i] for i in range(3)]
+        if all([spot == letter for spot in column_list]):
+            return True
                 
 
-        # check diagonals
+        # check diagonals. only if square in spot [0,2,4,6,8] will lead to a win.
+        if square in [0,2,4,6,8]:
+            if all([self.board[i] == letter for i in [0,4,8]]):
+                return True
+            
+            if all([self.board[i] == letter for i in [2,4,6]]):
+                return True
 
-        if all(self.board[i] == letter for i in [0,4,8]):
-
-        if all(self.board[i] == letter for i in [2,4,6]):
-
+        return False
 
 
 
 
 # Let's initiate a game
 
-def play("some variables",letter, print_board = True):
+def play(game,x_player,o_player, print_board = True):
+    if print_board == True:
+        game.gameboard_num()
 
+    # the game class need a initial parameter letter.
+    letter = "X" # starting letter
 
-    if print_borad == True:
+    while not game.win or len(game.available_spot) != 0:
+        if letter == 'X':
+            square = x_player.nextmove(game)
+        else:
+            square = o_player.nextmove(game)
+        
+        # In stead directly call take_a_move, I can call a condition 
+        if game.take_a_move(square,letter): # check if the take_a_move is valid, and 
+                                    # meantime calling the method triggering update the board
 
-        letter = "X"
-    # humanplayer take a move
+            if print_board:
+                print(letter + f' makes a move to spot {square}')
+                game.print_currentboard()
+                print(' ')
+        # after take a moving we should check if the game is over. check current winner
+            if game.winner: # winner is not None
+                print('The winner is '+ letter)
+                return letter # exit the function means end the game
+        #after one player take a move, we need to switch the player
+        letter = 'X' if letter == 'O' else 'O'      
+    
+
+    # need a while loop
+    if print_board:
+        print('It\'s a tie game')
+
 
 
     
---
+++ player.py
@@ -0,0 +1,48 @@
+#define a class as player (superclass)
+import random
+
+class player:
+    def __init__(self,letter):
+        self.letter = letter
+    
+
+
+    
+class humanplayer(player):
+
+    def __init__(self,letter):
+        super().__init__(letter)
+
+    def nextmove(self,game):
+        valid_move = False
+        # we need to choose a valid spot 
+        # a valid spot should be a number within the available list
+        spot_choose = input(self.letter + '\'s turn . Input move(0-8):')
+        # need valid input, using try
+        while valid_move != True:
+            try:
+                spot_choose = int(spot_choose) # if not integer, will raise a TypeError
+                if spot_choose not in game.available_spot():
+                    raise ValueError
+                valid_move = True
+
+            except ValueError:
+                print('Invalid input, please choose a number again!')
+
+        return spot_choose        
+
+
+
+class computerplayer(player):
+
+    def __init__(self,letter):
+        super().__init__(letter)
+
+    def nextmove(self,game):
+        # take a random available spot
+        spot_choose = random.choice(game.available_spot())
+        return spot_choose
+
+
+
+
